dfd6e9cf65c58de0d9e69d120d2ee88a72b47999
nzhul <dobromirivanov1@gmail.com>
2015-06-22 23:59:56 +0300

    Initial MVC Setup

---
+++ Dabravata/Dabravata.Models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dabravata.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public int Price { get; set; }
+        
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Dabravata/Dabravata.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dabravata.Web.Startup))]
+namespace Dabravata.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
