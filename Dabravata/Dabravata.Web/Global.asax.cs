94edaefc85411b5ad2d0e98512ff1313c4e833be
Dobromir Ivanov <dobromirivanov1@gmail.com>
2015-08-18 09:43:04 +0300

    Change in Utilities namespace

--- Dabravata/Dabravata.Web/Global.asax.cs
+++ Dabravata/Dabravata.Web/Global.asax.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Dabravata.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Utilities;
 
 namespace Dabravata.Web
 {
--- Dabravata/Utilities/BulgarianTimeModelBinder.cs
+++ Dabravata/Utilities/BulgarianTimeModelBinder.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
-namespace Utilities
+namespace Dabravata.Utilities
 {
     public class BulgarianTimeModelBinder : DefaultModelBinder
     {
